7efb4cdaa25b7193d7935c54890392ff05471a69
Joongwon Kim <danieljkim0118@gmail.com>
2020-04-22 20:53:27 -0500

    city buttons

--- WebApp/web/src/Components/Predictions/Predictions.js
+++ WebApp/web/src/Components/Predictions/Predictions.js
@@ -1,10 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Button} from 'react-bootstrap';
+import Select from 'react-select';
 
 class Predictions extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            placeholder: [],
+            cities: [],
+            features: []
+        }
+
+        this.showFeatures = this.showFeatures.bind(this);
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:5000/api/city",
+        {
+            method: "GET"
+        }
+        ).then(res => {
+            return res.send();
+        }, err => {
+            console.log(err);
+        }).then(cityList => {
+            if (!cityList) return;
+            let cityDivs = cityList.map((cityObj, i) => <Button id={"button-" + cityObj.city} onClick={() => this.showFeatures(cityObj.city)}>{cityObj.city}</Button>);
+            this.setState({
+                cities: cityDivs
+            });
+        }, err => {
+            console.log(err);
+        });
+    }
+
+    showFeatures(city) {
+        this.setState({
+            placeholder: city
+        })
+    }
+
     render () {
         return (
-            <div>
-                put your code for predictions here
+            <div className="predictions">
+                <div className="container">
+                    <div className="h5">U.S. Cities</div>
+                    <div className="cities-container">
+                        {this.state.cities}
+                    </div>
+                </div>
             </div>
         )
     }
