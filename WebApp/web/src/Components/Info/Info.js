791e32519c62f6e75c07ac7a054a3a527a0e6ce8
Matthew <matthew25kim@gmail.com>
2020-04-29 16:19:54 -0400

    home page

--- WebApp/web/src/Components/Info/Info.js
+++ WebApp/web/src/Components/Info/Info.js
@@ -12,30 +12,31 @@ class Info extends Component {
     render () {
         return (
             <div>
-                <Tab.Container id="left-tabs" defaultActiveKey="first">
+                <Tab.Container className="tab-container" id="left-tabs" defaultActiveKey="first">
                     <Row>
+                    <div class="col-1">
                         <Col lg={1}>
-                        <Nav variant="pills" className="flex-column">
-                            <Nav.Item>
-                                <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
+                            <Nav variant="pills" className="flex-column">
+                                <Nav.Item className="item">
+                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
                         </Col>
+                    </div>
+
+
                         <Col sm={9}>
                             <Tab.Content>
                                 <Tab.Pane eventKey="first">
                                 <Jumbotron>
                                     <h1>Welcome to Safe Roads!</h1>
                                     <p>
-                                        This is a simple hero unit, a simple jumbotron-style component for calling
-                                        extra attention to featured content or information.
+                                    Web application for CIS 550 (Databases & Info Systems) final project at the University of Pennsylvania with functionalities for querying and visualizing U.S. accident data with associated weather factors.
                                     </p>
-                                    <p>
-                                        <Button variant="primary">Learn more</Button>
-                                    </p>
+
                                 </Jumbotron>
                                 <div class="card-wrapper">
                                     <InfoCard className="cards" title={"Search and Show"} info={"hello"} img={search}/>
