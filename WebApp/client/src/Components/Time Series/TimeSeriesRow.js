e0ee2b4ddc93a550b4800d80c1f5d9a5c1681adb
Kevin Sun <kevinsun0@gmail.com>
2020-05-03 22:21:03 -0400

    add procfile

--- WebApp/client/src/Components/Time Series/TimeSeriesRow.js
+++ WebApp/client/src/Components/Time Series/TimeSeriesRow.js
@@ -8,8 +8,10 @@ class TimeSeriesRow extends React.Component {
             <div className="time-series-row">
                 <div className="year">{this.props.year}</div>
                 <div className="month">{this.props.month}</div>
-                <div className="id">{this.props.id}</div>
-                <div className="time">{this.props.time}</div>
+                {/* <div className="id">{this.props.id}</div>
+                <div className="time">{this.props.time}</div> */}
+                <div className="severity">{this.props.cum_severity}</div>
+                <div className="accidents">{this.props.cum_accidents}</div>
                 <div className="cum_severity">{this.props.cum_severity}</div>
                 <div className="cum_accidents">{this.props.cum_accidents}</div>
             </div>
