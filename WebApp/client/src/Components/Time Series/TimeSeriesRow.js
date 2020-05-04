ab2951dacdbe1827d8c2dc49a98ce17c3a5c6c7f
Kevin Sun <kevinsun0@gmail.com>
2020-05-03 23:58:39 -0400

    finish cumulative, update readme

--- WebApp/client/src/Components/Time Series/TimeSeriesRow.js
+++ WebApp/client/src/Components/Time Series/TimeSeriesRow.js
@@ -10,8 +10,8 @@ class TimeSeriesRow extends React.Component {
                 <div className="month">{this.props.month}</div>
                 {/* <div className="id">{this.props.id}</div>
                 <div className="time">{this.props.time}</div> */}
-                <div className="severity">{this.props.cum_severity}</div>
-                <div className="accidents">{this.props.cum_accidents}</div>
+                <div className="severity">{this.props.severity}</div>
+                <div className="accidents">{this.props.accidents}</div>
                 <div className="cum_severity">{this.props.cum_severity}</div>
                 <div className="cum_accidents">{this.props.cum_accidents}</div>
             </div>
